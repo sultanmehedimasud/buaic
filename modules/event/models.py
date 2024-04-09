8ee82e3f0645b4ed3b764e23e19e8a105ed2212b
Sultan Mehedi Masud <151965696+sultanmehedimasud@users.noreply.github.com>
2024-04-09 12:31:18 +0600

    Update models.py
    added end_date to the database

--- modules/event/models.py
+++ modules/event/models.py
@@ -8,10 +8,10 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
     venue = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     
     def get_event(self):
-        return str(self.id, self.title, self.description, self.date, self.venue, self.created_at)
-
+        return self.title
